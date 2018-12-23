0d145c3f579d9a372e1c54d8cbce03503e35cfbf
Alexis Lucas <alexis.lucas1@viacesi.fr>
2018-12-23 02:43:31 +0000

    Added jsoup and khttp as dependencies Implemented a first workflow to parse the first page

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
+    compile("khttp:khttp:0.1.0")
+    compile("org.jsoup:jsoup:1.10.3")
 }
 
 tasks.withType<KotlinCompile> {
