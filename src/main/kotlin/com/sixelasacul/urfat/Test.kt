35f28bfb57d038fa25d843f334bd5746955bfd9e
Alexis Lucas <alexis.lucas1@viacesi.fr>
2018-12-26 22:51:51 +0000

    Small refactors, trying to fix the bug making impossible to go next page

--- src/main/kotlin/com/sixelasacul/urfat/Test.kt
+++ src/main/kotlin/com/sixelasacul/urfat/Test.kt
@@ -13,23 +13,31 @@ fun main(args: Array<String>) {
     println(code)
 }
 
-fun browsePage(url: String, headers: Map<String, String>? = null, body: Map<String, String>? = null): String {
-    val document: Document = getDocument(url, headers, body)
+fun browsePage(url: String, body: Map<String, String>? = null): String {
+    val document: Document = getDocument(url, body)
     val code: String? = getCode(document)
-    if (code == null) {
+    return if (code == null) {
         val form: Element = document.select("form").first()
         val action: String = form.attr("action")
         val fields: Elements = form.select("input, select")
         val newBody = generateValues(fields)
-        val newHeaders: MutableMap<String, String> = if (headers == null) {
-            mutableMapOf()
-        } else {
-            headers as MutableMap<String, String>
-        }
-        newHeaders["Content-Type"] = "application/x-www-form-urlencoded"
-        return browsePage(buildUrl(url, action), newHeaders, newBody)
+        browsePage(buildUrl(url, action), newBody)
     } else {
-        return code
+        code
+    }
+}
+
+fun getDocument(url: String, headers: Map<String, String>? = null, body: Map<String, String>? = null): Document {
+    return if (headers != null && body != null) {
+        Jsoup.parse(
+            post(
+                url = url,
+                headers = mapOf("Content-Type" to "application/x-www-form-urlencoded"),
+                data = body
+            ).text
+        )
+    } else {
+        Jsoup.parse(get(url).text)
     }
 }
 
@@ -41,6 +49,20 @@ fun buildUrl(previousUrl: String, action: String): String {
         "$temp$action"
     } else {
         "$previousUrl$action"
+    }
+}
+
+fun getCode(document: Document): String? {
+    val codeSelection: Elements = document.select("#finishIncentiveHolder .ValCode")
+    if (codeSelection.size == 0) {
+        return null
+    } else {
+        val regex: Regex = Regex("Validation Code: (.+)")
+        val result: MatchResult? = regex.find(codeSelection.html())
+        if (result != null) {
+            return result.value
+        }
+        return null
     }
 }
 
@@ -82,11 +104,14 @@ fun valueForTextInput(input: Element): String {
 fun getRadioInputs(inputs: Elements, name: String): List<Element> =
     inputs.filter { radio -> radio.attr("name") === name }
 
-fun valueForRadioInput(inputs: Elements): String = valueForRadioInput(inputs.toList())
-
 fun valueForRadioInput(inputs: List<Element>): String = inputs[0].`val`()
 
-fun valueForHiddenInput(input: Element): String = input.`val`()
+fun valueForHiddenInput(input: Element): String {
+    return if (input.attr("name") == "JavaScriptEnabled")
+        "1"
+    else
+        input.`val`()
+}
 
 fun valueForSelectInput(input: Element): String {
     input.children().filter { it.tagName() === "option" }.forEach {
@@ -94,26 +119,4 @@ fun valueForSelectInput(input: Element): String {
             return it.`val`()
     }
     return ""
-}
-
-fun getCode(document: Document): String? {
-    val codeSelection: Elements = document.select("#finishIncentiveHolder .ValCode")
-    if (codeSelection.size == 0) {
-        return null
-    } else {
-        val regex: Regex = Regex("Validation Code: (.+)")
-        val result: MatchResult? = regex.find(codeSelection.html())
-        if (result != null) {
-            return result.value
-        }
-        return null
-    }
-}
-
-fun getDocument(url: String, headers: Map<String, String>? = null, body: Map<String, String>? = null): Document {
-    if (headers != null && body != null) {
-        return Jsoup.parse(post(url = url, headers = headers, json = body).text)
-    } else {
-        return Jsoup.parse(get(url).text)
-    }
 }
